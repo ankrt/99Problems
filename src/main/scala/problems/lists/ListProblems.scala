c2b7025460870c80d6f942adb478564f759c52d5
ankrt <ankrt@mail.com>
2017-01-19 10:17:45 +0000

    [Lists] Number the problems

--- src/main/scala/problems/lists/ListProblems.scala
+++ src/main/scala/problems/lists/ListProblems.scala
@@ -2,43 +2,43 @@ package problems.lists
 
 trait ListProblems {
 
-  /** Find the last element of a list
+  /** 01 Find the last element of a list
     */
   def last[A](ls: List[A]): A
 
-  /** Find the last but one element of a list
+  /** 02 Find the last but one element of a list
     */
   def penultimate[A](ls: List[A]): A
 
-  /** Find the nth element of a list
+  /** 03 Find the nth element of a list
     */
   def nth[A](n: Int, ls: List[A]): Option[A]
 
-  /** Find the number of elements of a list
+  /** 04 Find the number of elements of a list
     */
   def length[A](ls: List[A]): Int
 
-  /** Reverse a list
+  /** 05 Reverse a list
     */
   def reverse[A](ls: List[A]): List[A]
 
-  /** Test whether a list is a palindrome
+  /** 06 Test whether a list is a palindrome
     */
   def isPalindrome[A](ls: List[A]): Boolean
 
-  /** Flatten a nested list structure
+  /** 07 Flatten a nested list structure
     */
   def flatten(ls: List[Any]): List[Any]
 
-  /** Eliminate consecutive duplicates of list elements
+  /** 08 Eliminate consecutive duplicates of list elements
     */
   def compress[A](ls: List[A]): List[A]
 
-  /** Pack consecutive duplicates of list elements into sublists
+  /** 09 Pack consecutive duplicates of list elements into sublists
     */
   def pack[A](ls: List[A]): List[List[A]]
 
-  /** Run-length encoding of a list
+  /** 10 Run-length encoding of a list
     */
   def encode[A](list: List[A]): List[(Int, A)]
 }
