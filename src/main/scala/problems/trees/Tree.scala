ad153dea70c14e0056eac11fb3d0bae26f5c18ce
ankrt <ankrt@mail.com>
2017-04-24 16:07:21 +0100

    [Trees] Generate balanced binary trees.
    Straying a little away from the question because this returns only a single tree
    rather than all possible trees.
    
    Also implement the toString method for trees.

--- src/main/scala/problems/trees/Tree.scala
+++ src/main/scala/problems/trees/Tree.scala
@@ -1,17 +1,15 @@
 package problems.trees
 
 sealed abstract class Tree[+T] {
-
   def isMirrorOf[V](that: Tree[V]): Boolean
   def isSymmetric: Boolean
-
 }
 
 case object End extends Tree[Nothing] {
-
   def isMirrorOf[V](that: Tree[V]): Boolean = that == End
   def isSymmetric: Boolean = true
 
+  override def toString: String = "."
 }
 
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
@@ -19,17 +17,43 @@ case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   def isMirrorOf[V](that: Tree[V]): Boolean = ???
   def isSymmetric: Boolean = ???
 
-}
+  override def toString: String = {
+    "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+  }
 
+}
 
 object Node {
 
   def apply[T](value: T): Node[T] = Node(value, End, End)
-  
+
 }
 
 object Tree {
 
-  def balanced[T](n: Int, v: T): Tree[T] = ???
+  /**
+    * Construct a balanced binary tree.
+    *
+    * In a balanced binary tree the following property holds for every node:
+    * the number of nodes in its left subtree and its right subtree are almost
+    * equal, which means their difference is not greater than one.
+    *
+    * @param nodes the number of nodes to generate
+    * @param value the value to put in each node
+    * @tparam T the type of the value
+    * @return a balanced binary tree
+    */
+  def balanced[T](nodes: Int, value: T): Tree[T] = nodes match {
+    case n if n < 1 => End
+    case n if n % 2 == 1 => {
+      val subtree = balanced(n / 2, value)
+      Node(value, subtree, subtree)
+    }
+    case n if n % 2 == 0 => {
+      val greaterSubtree = balanced((n - 1) / 2, value)
+      val lesserSubtree = balanced((n - 1) / 2 + 1, value)
+      Node(value, lesserSubtree, greaterSubtree)
+    }
+  }
 
 }
